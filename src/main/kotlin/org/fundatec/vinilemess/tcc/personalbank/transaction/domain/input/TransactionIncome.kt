8aae539bbca339fe3bb55dfbeeebb49fdab1f34a
ViniLemess <viniciuslemescaetano@gmail.com>
2023-04-13 01:03:20 -0300

    Cria validacoes customizadas para objetos de entrada do sistema

--- src/main/kotlin/org/fundatec/vinilemess/tcc/personalbank/transaction/domain/input/TransactionIncome.kt
+++ src/main/kotlin/org/fundatec/vinilemess/tcc/personalbank/transaction/domain/input/TransactionIncome.kt
@@ -1,9 +1,11 @@
 package org.fundatec.vinilemess.tcc.personalbank.transaction.domain.input
 
 import org.fundatec.vinilemess.tcc.personalbank.transaction.domain.TransactionOperation
+import org.fundatec.vinilemess.tcc.personalbank.validation.FieldValidator
 import org.springframework.format.annotation.DateTimeFormat
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.*
 
 class TransactionIncome(
     val amount: BigDecimal,
@@ -11,4 +13,12 @@ class TransactionIncome(
     val incomeDate: LocalDate
 ) {
     private val transactionOperation = TransactionOperation.INCOME
+
+    fun validateRequest() {
+        FieldValidator()
+            .addConstraint(isAmountNegative(), "amount", "Expenses cannot be negative or 0")
+            .validate()
+    }
+
+    fun isAmountNegative() = { -> Objects.isNull(amount) || amount < BigDecimal.ZERO }
 }
--- src/main/kotlin/org/fundatec/vinilemess/tcc/personalbank/user/domain/request/UserRequest.kt
+++ src/main/kotlin/org/fundatec/vinilemess/tcc/personalbank/user/domain/request/UserRequest.kt
@@ -1,7 +1,7 @@
 package org.fundatec.vinilemess.tcc.personalbank.user.domain.request
 
 import org.apache.logging.log4j.util.Strings
-import org.fundatec.vinilemess.tcc.personalbank.infra.exception.InvalidBodyException
+import org.fundatec.vinilemess.tcc.personalbank.validation.FieldValidator
 
 private const val EMAIL_REGEX = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"
 
@@ -10,14 +10,14 @@ class UserRequest(
     val email: String
 ) {
     fun validateRequest() {
-        val violations = mutableMapOf<String, String>()
-
-        if (Strings.isBlank(name)) violations["name"] = "Cannot be blank"
-        if (Strings.isBlank(email) || !email.matches(Regex(EMAIL_REGEX))) violations["email"] = "Invalid email"
-
-        if (violations.isNotEmpty()) throw InvalidBodyException(
-            "Invalid user json body, violations must be corrected",
-            violations
-        )
+        FieldValidator()
+            .addConstraint(isNameBlank(), "name", "Cannot be blank")
+            .addConstraint(isEmailValid(), "email", "Invalid email")
+            .validate("Invalid user json body, violations must be corrected")
     }
+
+    private fun isNameBlank() = { -> Strings.isBlank(name) }
+
+    private fun isEmailValid() = { -> Strings.isBlank(email) || !email.matches(Regex(EMAIL_REGEX)) }
+
 }
