d91728ad6863bed9cdd92072cda4507502f26a98
ViniLemess <viniciuslemescaetano@gmail.com>
2023-04-28 21:37:20 -0300

    Cria interceptor para validar requests

---
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/tcc/fintrack/infra/Request.kt
@@ -0,0 +1,6 @@
+package org.fundatec.vinilemess.tcc.fintrack.infra
+
+interface Request {
+
+    fun validateRequest()
+}
---
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/tcc/fintrack/infra/RequestValidatorAdvice.kt
@@ -0,0 +1,39 @@
+package org.fundatec.vinilemess.tcc.fintrack.infra
+
+import org.springframework.core.MethodParameter
+import org.springframework.http.HttpInputMessage
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter
+import java.lang.reflect.Type
+
+@ControllerAdvice
+class RequestValidatorAdvice : RequestBodyAdviceAdapter() {
+
+    /**
+     * This method always returns true, meaning that the afterBodyRead
+     * validation should be aplied to all requests that have a body.
+     */
+    override fun supports(
+        methodParameter: MethodParameter,
+        targetType: Type,
+        converterType: Class<out HttpMessageConverter<*>>
+    ) = true
+
+    override fun afterBodyRead(
+        body: Any,
+        inputMessage: HttpInputMessage,
+        parameter: MethodParameter,
+        targetType: Type,
+        converterType: Class<out HttpMessageConverter<*>>
+    ): Any {
+        if (isImplementingRequest(body)) {
+            (body as Request).validateRequest()
+        }
+        return super.afterBodyRead(body, inputMessage, parameter, targetType, converterType)
+    }
+
+    private fun isImplementingRequest(body: Any): Boolean {
+        return Request::class.java.isAssignableFrom(body.javaClass)
+    }
+}
