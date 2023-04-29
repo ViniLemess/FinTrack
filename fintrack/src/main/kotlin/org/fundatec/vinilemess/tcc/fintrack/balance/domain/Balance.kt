c389c320bf9f7d94a8c6185ed239a95d21f84504
ViniLemess <viniciuslemescaetano@gmail.com>
2023-04-29 15:49:32 -0300

    Remove entidade e repositorio nao utilizados

--- fintrack/src/main/kotlin/org/fundatec/vinilemess/tcc/fintrack/balance/domain/Balance.kt
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/tcc/fintrack/balance/domain/Balance.kt
@@ -1,20 +1,6 @@
 package org.fundatec.vinilemess.tcc.fintrack.balance.domain
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.format.annotation.DateTimeFormat
 import java.math.BigDecimal
 import java.time.LocalDate
 
-@Document(collection = "balances")
-data class Balance(
-        @Id
-        val id: String?,
-        val userSignature: String,
-        val amount: BigDecimal,
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-        val date: LocalDate)
-
 data class BalanceResponse(val amount: BigDecimal, val date: LocalDate)
-
-fun Balance.toBalanceResponse() = BalanceResponse(this.amount, this.date)
