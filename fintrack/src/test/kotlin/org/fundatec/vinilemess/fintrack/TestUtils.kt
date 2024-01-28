34313e5af1fe22fe5163fcf30ac4531a66f0c5d4
vinilemess <viniciuslemescaetano@gmail.com>
2024-01-28 17:54:31 -0300

    Rename test package paths
    The package paths for all test cases have been renamed to remove unnecessary 'tcc' directory.

---
+++ fintrack/src/test/kotlin/org/fundatec/vinilemess/fintrack/TestUtils.kt
@@ -0,0 +1,29 @@
+package org.fundatec.vinilemess.fintrack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.fail
+import org.springframework.http.ProblemDetail
+
+private const val defaultMessage = "The provided data contains violations that must be corrected"
+
+fun assertProblemDetailsContainsViolation(problemDetail: ProblemDetail, field: String, msg: String = defaultMessage) {
+    assertThat { ->
+        Assertions.assertEquals(400, problemDetail.status)
+        Assertions.assertEquals(msg, problemDetail.detail)
+        Assertions.assertEquals("Invalid request body", problemDetail.title)
+        Assertions.assertNotNull(problemDetail.properties)
+        problemDetail.properties?.let { assertContainsViolation(it, field) }
+    }
+}
+
+private fun assertContainsViolation(properties: Map<String, Any>, violation: String) {
+    properties["invalidFields"]?.let { invalidFields: Any ->
+        if ((invalidFields is Map<*, *>) && invalidFields.all { it.key is String && it.value is String }) {
+            assertTrue(invalidFields.containsKey(violation)) { -> "no value found for violation key $violation" }
+        } else {
+            fail("could not apply cast to Map<String, String> for $invalidFields")
+        }
+    }
+}
---
+++ fintrack/src/test/kotlin/org/fundatec/vinilemess/fintrack/transaction/service/TransactionServiceTest.kt
@@ -0,0 +1,86 @@
+package org.fundatec.vinilemess.fintrack.transaction.service
+
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.fundatec.vinilemess.fintrack.*
+import org.fundatec.vinilemess.fintrack.transaction.domain.Transaction
+import org.fundatec.vinilemess.fintrack.transaction.domain.enums.Recurrence
+import org.fundatec.vinilemess.fintrack.transaction.domain.enums.TransactionOperation
+import org.fundatec.vinilemess.fintrack.transaction.domain.request.RecurrentTransactionRequest
+import org.fundatec.vinilemess.fintrack.transaction.domain.request.TransactionRequest
+import org.fundatec.vinilemess.fintrack.transaction.repository.TransactionRepository
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class TransactionServiceTest {
+
+    private val transactionRepository: TransactionRepository = mockk()
+    private val transactionService = TransactionService(transactionRepository)
+
+    @Test
+    fun `Should not throw exception when transact is called`() {
+        val transaction = TransactionRequest(testPositiveAmount, testDate, testDescription, TransactionOperation.INCOME)
+        justRun { transactionRepository.save(any()) }
+
+        assertDoesNotThrow { transactionService.transact(transaction, userSignature) }
+
+        verify { transactionRepository.save(any()) }
+    }
+
+    @Test
+    fun `Should call repository save 1 time when transactRecurrence is called for any recurrenceCount`() {
+        val recurrentTransaction = RecurrentTransactionRequest(
+            testPositiveAmount,
+            testDate,
+            testDescription,
+            TransactionOperation.INCOME,
+            Recurrence.MONTHLY,
+            1,
+            10
+        )
+        justRun { transactionRepository.saveAll(any()) }
+
+        assertDoesNotThrow { transactionService.transactRecurrence(recurrentTransaction, userSignature) }
+
+        verify { transactionRepository.saveAll(any()) }
+    }
+
+    @Test
+    fun `Should return transactions mapped to response when method listTransactionsBeforeDateByUserSignature is called`() {
+        val transactions = listOf(
+            Transaction(testId, userSignature, null, testDate, testPositiveAmount, testDescription, TransactionOperation.INCOME),
+            Transaction(testId, userSignature, null, testDate, testPositiveAmount, testDescription, TransactionOperation.INCOME),
+            Transaction(testId, userSignature, null, testDate, testPositiveAmount, testDescription, TransactionOperation.INCOME)
+        )
+        every { transactionRepository.findTransactionsBeforeDateByUserSignature(userSignature, testDate) } returns transactions
+
+        val result = transactionService.listTransactionsBeforeDateByUserSignature(userSignature, testDate)
+
+        assertThat(result).allSatisfy {
+            assertEquals(testId, it.id)
+            assertEquals(userSignature, it.userSignature)
+            assertNull(it.recurrenceId)
+            assertEquals(testDate, it.date)
+            assertEquals(testPositiveAmount, it.amount)
+            assertEquals(testDescription, it.description)
+            assertEquals(TransactionOperation.INCOME, it.transactionOperation)
+        }
+
+        verify { transactionRepository.findTransactionsBeforeDateByUserSignature(userSignature, testDate) }
+    }
+
+    @Test
+    fun `Should call repository delete without throwing exception when deleteById is called`() {
+        val idList = listOf(testId)
+        justRun { transactionRepository.deleteByIdList(idList = idList) }
+
+        assertDoesNotThrow { transactionService.deleteTransactionsByIdList(idList = listOf(testId)) }
+
+        verify { transactionRepository.deleteByIdList(idList = listOf(testId)) }
+    }
+}
