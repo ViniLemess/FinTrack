4791597c5b46cebb4b02927e8ce57f7b9bd464eb
vinilemess <viniciuslemescaetano@gmail.com>
2024-03-10 02:53:52 -0300

    Improve exception handling and security configuration
    This update enhances the security configuration of the application by adding a Customizer for exception handling. Additionally, an expand global exception handler has been defined to catch AuthenticationException and other unknown exceptions, providing a more detailed error response for the clients. The readability of the security configuration class has been improved as well with some formatting adjustments.

--- fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/infra/GlobalExceptionHandler.kt
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/infra/GlobalExceptionHandler.kt
@@ -1,7 +1,44 @@
 package org.fundatec.vinilemess.fintrack.infra
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.AuthenticationException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.net.URI
 
 @RestControllerAdvice
-class GlobalExceptionHandler : ResponseEntityExceptionHandler()
+class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(AuthenticationException::class)
+    fun handleAuthenticationException(
+        authenticationException: AuthenticationException,
+        request: HttpServletRequest
+    ): ResponseEntity<ProblemDetail> {
+        logger.error("Authentication Error", authenticationException)
+        val unauthorizedMessage = authenticationException.message ?: "Unauthorized, please make sure you are authenticated correctly"
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, unauthorizedMessage)
+        problemDetail.instance = URI.create(request.requestURI)
+        problemDetail.title = "Authentication Error"
+
+        return ResponseEntity.of(problemDetail)
+            .build()
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleAuthenticationException(
+        exception: Exception?,
+        request: HttpServletRequest
+    ): ResponseEntity<ProblemDetail> {
+        logger.error("Unknown error", exception)
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Error :(")
+        problemDetail.instance = URI.create(request.requestURI)
+        problemDetail.title = "Internal Server Error"
+
+        return ResponseEntity.of(problemDetail)
+            .build()
+    }
+}
--- fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/security/configuration/SecurityConfiguration.kt
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/security/configuration/SecurityConfiguration.kt
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -24,50 +25,54 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity
 class SecurityConfiguration(
-        private val logoutHandler: LogoutHandler,
-        private val jwtAuthenticationFilter: JwtAuthenticationFilter,
-        private val authenticationProvider: AuthenticationProvider
+    private val logoutHandler: LogoutHandler,
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationProvider: AuthenticationProvider
 ) {
 
-    private val whiteListUrls: Array<String> = arrayOf("/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/auths/authenticate",
-            "/actuator/health",
-            "/swagger-resources/**",
-            "/login",
-            "/refresh",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/webjars/**",
-            "/swagger-ui.html")
+    private val whiteListUrls: Array<String> = arrayOf(
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/v3/api-docs/**",
+        "/swagger-resources",
+        "/auths/authenticate",
+        "/actuator/health",
+        "/swagger-resources/**",
+        "/login",
+        "/refresh",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-ui.html"
+    )
 
     @Bean
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
-                .csrf { it.disable() }
-                .authorizeHttpRequests { req ->
-                    req.requestMatchers(*whiteListUrls)
-                            .permitAll()
-                            .requestMatchers(HttpMethod.POST, "/users").hasAnyAuthority(
-                                    Permission.USER_CREATE.getPermission()
-                            )
-                            .anyRequest()
-                            .authenticated()
-                }
-                .sessionManagement { session: SessionManagementConfigurer<HttpSecurity> ->
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                }
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
-                .logout { logout: LogoutConfigurer<HttpSecurity> ->
-                    logout.logoutUrl("/api/v1/sessions/logout")
-                            .addLogoutHandler(logoutHandler)
-                            .logoutSuccessHandler { _: HttpServletRequest?, _: HttpServletResponse?, _: Authentication? ->
-                                SecurityContextHolder.clearContext()
-                            }
-                }.build()
+            .csrf { it.disable() }
+            .authorizeHttpRequests { req ->
+                req.requestMatchers(*whiteListUrls)
+                    .permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users").hasAnyAuthority(
+                        Permission.USER_CREATE.getPermission()
+                    )
+                    .anyRequest()
+                    .authenticated()
+            }
+            .sessionManagement { session: SessionManagementConfigurer<HttpSecurity> ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .logout { logout: LogoutConfigurer<HttpSecurity> ->
+                logout.logoutUrl("/api/v1/sessions/logout")
+                    .addLogoutHandler(logoutHandler)
+                    .logoutSuccessHandler { _: HttpServletRequest?, _: HttpServletResponse?, _: Authentication? ->
+                        SecurityContextHolder.clearContext()
+                    }
+            }
+            .exceptionHandling(Customizer.withDefaults())
+            .build()
     }
 }
