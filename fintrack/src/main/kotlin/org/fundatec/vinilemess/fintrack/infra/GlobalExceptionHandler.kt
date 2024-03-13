dae6ff5c6f765c821f3f2e60742ab7ae7c9152dd
vinilemess <viniciuslemescaetano@gmail.com>
2024-03-13 00:36:45 -0300

    Improve error handling and add Access Denied exception
    Refactored the code for error handling in the GlobalExceptionHandler class in order to use a builder pattern. An Access Denied exception handler has been added to provide more specific feedback when users try to reach unauthorized resources.

--- fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/infra/GlobalExceptionHandler.kt
+++ fintrack/src/main/kotlin/org/fundatec/vinilemess/fintrack/infra/GlobalExceptionHandler.kt
@@ -8,6 +8,7 @@ import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.security.access.AccessDeniedException
 import java.net.URI
 
 @RestControllerAdvice
@@ -19,27 +20,67 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
         request: HttpServletRequest
     ): ResponseEntity<ProblemDetail> {
         logger.error("Authentication Error", authenticationException)
-        val unauthorizedMessage =
-            authenticationException.message ?: "Unauthorized, please make sure you are authenticated correctly"
-        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, unauthorizedMessage)
-        problemDetail.instance = URI.create(request.requestURI)
-        problemDetail.title = "Authentication Error"
+        val unauthorizedMessage = authenticationException.message ?: "Unauthorized, please make sure you are authenticated correctly"
 
-        return ResponseEntity.of(problemDetail)
+        return problemDetailForStatusBuilder(HttpStatus.UNAUTHORIZED)
+            .withDetail(unauthorizedMessage)
+            .withInstance(request.requestURI)
+            .withTitle("Authentication Error")
             .build()
     }
 
     @ExceptionHandler(Exception::class)
-    fun handleAuthenticationException(
-        exception: Exception?,
+    fun handleException(
+        exception: Exception,
         request: HttpServletRequest
     ): ResponseEntity<ProblemDetail> {
         logger.error("Unknown error", exception)
-        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Error :(")
-        problemDetail.instance = URI.create(request.requestURI)
-        problemDetail.title = "Internal Server Error"
 
-        return ResponseEntity.of(problemDetail)
+        return problemDetailForStatusBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+            .withDetail("Unknown Error :(")
+            .withInstance(request.requestURI)
+            .withTitle("Internal Server Error")
             .build()
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(
+        exception: AccessDeniedException,
+        request: HttpServletRequest
+    ): ResponseEntity<ProblemDetail> {
+        logger.error("Access Denied", exception)
+
+        return problemDetailForStatusBuilder(HttpStatus.FORBIDDEN)
+            .withTitle("Forbidden Access")
+            .withDetail(exception.message?: "Access Denied")
+            .withInstance(request.requestURI)
+            .build()
+    }
+
+    private fun problemDetailForStatusBuilder(httpStatus: HttpStatus): ProblemDetailsResponseEntityBuilder {
+        return ProblemDetailsResponseEntityBuilder(ProblemDetail.forStatus(httpStatus))
+    }
+
+    private class ProblemDetailsResponseEntityBuilder(val problemDetail: ProblemDetail) {
+
+        fun withTitle(title: String): ProblemDetailsResponseEntityBuilder {
+            problemDetail.title = title
+            return this
+        }
+
+        fun withDetail(detail: String): ProblemDetailsResponseEntityBuilder {
+            problemDetail.detail = detail
+            return this
+        }
+
+        fun withInstance(uri: String): ProblemDetailsResponseEntityBuilder {
+            problemDetail.instance = URI.create(uri)
+            return this
+        }
+
+        fun build(): ResponseEntity<ProblemDetail> {
+            return ResponseEntity.of(this.problemDetail)
+                .build()
+        }
     }
 }
