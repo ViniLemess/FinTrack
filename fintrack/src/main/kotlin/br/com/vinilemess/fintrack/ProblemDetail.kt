7fe2e99fef14e3b7fab3b97c84b6ba91cae0480f
ViniLemess <viniciuslemescaetano@gmail.com>
2024-10-12 04:13:43 -0300

    Refactor instance field in ProblemDetail
    Changed the instance field from URI to String to simplify serialization. Removed @Contextual annotation as it is no longer necessary.

--- fintrack/src/main/kotlin/br/com/vinilemess/fintrack/ProblemDetail.kt
+++ fintrack/src/main/kotlin/br/com/vinilemess/fintrack/ProblemDetail.kt
@@ -1,16 +1,13 @@
 package br.com.vinilemess.fintrack
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import java.net.URI
 
 @Serializable
 data class ProblemDetail(
     val title: String,
     val status: Int,
     val detail: String,
-    @Contextual
-    val instance: URI,
+    val instance: String,
     val properties: Map<String, String> = emptyMap()
 ) {
 }
