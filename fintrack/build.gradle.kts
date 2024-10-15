c5160c521e6787826d71215e28752d9e1fe12e5b
ViniLemess <viniciuslemescaetano@gmail.com>
2024-10-15 00:06:50 -0300

    Refactor property names and add TestContainers dependencies
    Renamed properties in `build.gradle.kts` and `gradle.properties` to follow camelCase convention. Added TestContainers dependencies to enhance testing capabilities.

--- fintrack/build.gradle.kts
+++ fintrack/build.gradle.kts
@@ -1,12 +1,13 @@
 
-val kotlin_version: String by project
-val logback_version: String by project
-val swagger_codegen_version = "1.0.36"
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val swaggerCodegenVersion = "1.0.36"
 val kodeinDiVersion = "7.22.0"
 val mockkVersion = "1.13.12"
 val kotlinxSerializationVersion = "1.7.3"
 val junitVersion = "5.8.1"
 val mongodbKotlinVersion = "5.2.0"
+val testContainersVersion = "1.20.2"
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -35,11 +36,11 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
     // Documentation
     implementation("io.ktor:ktor-server-openapi")
-    implementation("io.swagger.codegen.v3:swagger-codegen-generators:$swagger_codegen_version")
+    implementation("io.swagger.codegen.v3:swagger-codegen-generators:$swaggerCodegenVersion")
 
     // DI
     implementation("org.kodein.di:kodein-di:$kodeinDiVersion")
@@ -60,7 +61,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("io.ktor:ktor-server-tests")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+    testImplementation("org.testcontainers:mongodb:$testContainersVersion")
 }
 
 tasks.named<Test>("test") {
