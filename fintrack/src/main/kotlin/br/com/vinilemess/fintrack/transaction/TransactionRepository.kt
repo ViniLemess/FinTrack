61871f97d02eabf8c2a1437045c69c114aa15ca8
ViniLemess <viniciuslemescaetano@gmail.com>
2024-10-19 02:49:02 -0300

    Refactor transaction save logic
    Streamlined `saveTransaction` method in `TransactionService` and added `copyWithId` method to set transaction ID after insertion in `TransactionRepository`. This increases code readability and micro-optimizes the transaction persistence flow.

--- fintrack/src/main/kotlin/br/com/vinilemess/fintrack/transaction/TransactionRepository.kt
+++ fintrack/src/main/kotlin/br/com/vinilemess/fintrack/transaction/TransactionRepository.kt
@@ -4,17 +4,17 @@ import com.mongodb.client.model.Filters.eq
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.toList
+import org.bson.BsonObjectId
 import org.bson.BsonValue
+import org.bson.types.ObjectId
 
 class TransactionRepository(private val mongoDatabase: MongoDatabase) {
     private val transactions = this.mongoDatabase.getCollection<Transaction>("transactions")
 
-    suspend fun saveTransaction(transaction: Transaction): Transaction? =
-        transactions.insertOne(transaction).insertedId?.let { findTransactionById(it) }
+    suspend fun saveTransaction(transaction: Transaction): Transaction? {
+        return transactions.insertOne(transaction).insertedId?.let { transaction.copyWithId(it.asObjectId()) }
+    }
 
     suspend fun findTransactionsBySignature(transactionSignature: String): List<Transaction> =
         transactions.find(eq("transactionSignature", transactionSignature)).toList()
-
-    private suspend fun findTransactionById(id: BsonValue): Transaction =
-        transactions.find(eq("_id", id)).first()
 }
--- fintrack/src/main/kotlin/br/com/vinilemess/fintrack/transaction/TransactionService.kt
+++ fintrack/src/main/kotlin/br/com/vinilemess/fintrack/transaction/TransactionService.kt
@@ -7,19 +7,20 @@ import br.com.vinilemess.fintrack.error.ProblemDetail
 
 class TransactionService(private val transactionRepository: TransactionRepository) {
 
-    suspend fun saveTransaction(createTransactionRequest: CreateTransactionRequest): ApiResult<TransactionResponse> {
-        val newTransaction = createTransactionFromRequest(createTransactionRequest)
-        return transactionRepository.saveTransaction(newTransaction)
-            ?.toResponse()
-            ?.let { Success(it) }
-            ?: Failure(
-                ProblemDetail(
-                    title = "Internal Error",
-                    status = 500,
-                    detail = "Something unexpected happened",
+    suspend fun saveTransaction(createTransactionRequest: CreateTransactionRequest): ApiResult<TransactionResponse> =
+        createTransactionRequest.toTransaction().let { transaction ->
+            transactionRepository.saveTransaction(transaction)
+                ?.toResponse()
+                ?.let { Success(it) }
+                ?: Failure(
+                    ProblemDetail(
+                        title = "Internal Error",
+                        status = 500,
+                        detail = "Something unexpected happened",
+                    )
                 )
-            )
-    }
+        }
+
 
     suspend fun findTransactionsBySignature(id: String): Success<List<TransactionResponse>> =
         Success(transactionRepository.findTransactionsBySignature(id).map {
